78fb8e022f1b71a13e3a5add237d38fdca062e25
gabriel <alves.go@protonmail.com>
2021-08-31 17:28:08 +0200

    Initial commit

---
+++ assignmentA0.py
@@ -0,0 +1,11 @@
+"""
+Name: Gabriel Oliveira Alves
+"""
+
+# List of course syllabus
+important_stuff = ["debugging", "least squares approximation",
+"convergence analysis", "numerical integration", "monte carlo"]
+
+# Prints the elements from the variable above using the {} .format() syntax
+for stuff in important_stuff:
+    print("I will learn something about {output}!".format(output=stuff))
